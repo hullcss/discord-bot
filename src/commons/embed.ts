d5f85f3ef75b9ffb3af11a0f5db328191d1f3409
Ash Entwisle <ash.entwisle@protonmail.com>
2024-12-29 16:48:20 +0000

    added more embed stuff and changed the msg delete thing
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- src/commons/embed.ts
+++ src/commons/embed.ts
@@ -1,18 +1,93 @@
 import { EmbedBuilder } from "@discordjs/builders";
-import type { Command } from "@sapphire/framework";
+import { Command } from "@sapphire/framework";
+import { ChatInputCommandInteraction, Message } from "discord.js";
 
-export function embedHelper(
-  interaction: Command.ChatInputCommandInteraction,
+// function rawEmbedHelper(
+//   author: string,
+//   url: string,
+//   title: string,
+//   description: string,
+// ): EmbedBuilder {
+//   return new EmbedBuilder()
+//     .setColor(0x0099ff)
+//     .setTitle(title)
+//     .setDescription(description)
+//     .setFooter({
+//       text: `@${author}`,
+//       iconURL: url,
+//     })
+//     .setTimestamp();
+// }
+
+// function messageEmbedHelper(
+//   interaction: Message,
+//   title: string,
+//   description: string,
+// ): EmbedBuilder {
+//   return new EmbedBuilder()
+//     .setColor(0x0099ff)
+//     .setTitle(title)
+//     .setDescription(description)
+//     .setFooter({
+//       text: `@${interaction}`,
+//       iconURL: interaction.user.displayAvatarURL(),
+//     })
+//     .setTimestamp();
+// }
+
+// function interactionEmbedHelper(
+//   interaction: Command.ChatInputCommandInteraction,
+//   title: string,
+//   description: string,
+// ): EmbedBuilder {
+//   return embedHelper(
+//     interaction.user.tag,
+//     interaction.user.displayAvatarURL(),
+//     title,
+//     description,
+//   );
+// }
+
+function embedHelper(
   title: string,
   description: string,
+  data?:
+    | Message
+    | Command.ChatInputCommandInteraction
+    | { name: string; avatar: string },
 ): EmbedBuilder {
-  return new EmbedBuilder()
-    .setColor(0x0099ff)
+  // switch case to determine the type of data
+
+  const embed = new EmbedBuilder()
+    .setColor(0x000000)
     .setTitle(title)
     .setDescription(description)
-    .setFooter({
-      text: `@${interaction.user.tag}`,
-      iconURL: interaction.user.displayAvatarURL(),
-    })
     .setTimestamp();
+
+  // return embed;
+
+  if (data instanceof Message) {
+    embed.setFooter({
+      text: `@${data.author.tag}`,
+      iconURL: data.author.displayAvatarURL(),
+    });
+  } else if (data instanceof ChatInputCommandInteraction) {
+    embed.setFooter({
+      text: `@${data.user.tag}`,
+      iconURL: data.user.displayAvatarURL(),
+    });
+  } else if (data) {
+    if (data.name && data.avatar) {
+      embed.setFooter({
+        text: `@${data.name}`,
+        iconURL: data.avatar,
+      });
+    }
+  }
+
+  embed.setF;
+
+  return embed;
 }
+
+export { embedHelper };
--- src/listeners/accepted_coc.ts
+++ src/listeners/accepted_coc.ts
@@ -1,5 +1,7 @@
 import { Events, Listener } from "@sapphire/framework";
 import { Message, MessageFlags } from "discord.js";
+import { embedHelper } from "../commons/embed";
+import type { EmbedBuilder } from "discord.js";
 
 export class HasSenderAcceptedCOCListener extends Listener {
   public constructor(
@@ -21,11 +23,28 @@ export class HasSenderAcceptedCOCListener extends Listener {
 
     // check if author has the role HULLCSS_ROLE_COC
     if (!message.member?.roles.cache.has(process.env.HULLCSS_ROLE_COC!)) {
-      const msg = `
-        Thanks for participating in the community, You need to accept the Code of Conduct to chat in the server.
-        Please accept the CoC in the [#roles](https://discord.com/channels/427865794467069962/973982410612027503) channel.
-        You won't be able to chat in the server until you accept the CoC.
-        `;
+      // const msg = `
+      //   Thanks for participating in the community, You need to accept the Code of Conduct to chat in the server.
+      //   Please accept the CoC in the [#roles](https://discord.com/channels/427865794467069962/973982410612027503) channel.
+      //   You won't be able to chat in the server until you accept the CoC.`;
+
+
+      let url: string = "https://discord.com/channels/427865794467069962/973982410612027503";
+      let msg: string = "";
+
+      msg += "Thanks for participating in the in the community, however to send messages, you ***must*** accept the Code of Conduct. ";
+      msg += `This can be found in the [#roles](${url}) channel in our discord. \n`;
+      msg += "If you need a hand, please feel free to message an exec. \n\n"
+      msg += `> \`${message.content}\``
+
+
+      const e = embedHelper(
+        "Please Accept the Code of Conduct",
+        msg,
+        message,
+      );
+
+      e.setURL(url);
 
       // TODO: it doesnt see this as ephemeral
       // let r = message.reply({
@@ -39,7 +58,7 @@ export class HasSenderAcceptedCOCListener extends Listener {
       message.delete();
 
       message.author.send({
-        content: msg,
+        embeds: [e],
       });
     }
   }
